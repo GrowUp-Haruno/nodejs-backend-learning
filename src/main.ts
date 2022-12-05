bda9cba163c0eef36c56cf28d84bd82c81b6f022
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-12-05 14:40:53 +0900

    基本実装

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,52 @@
-import * as http from 'http';
+import express, { Request } from 'express';
 
-// class Server {
-//   #httpServer
-//   constructor() {
-//     this.#httpServer = http.createServer
-//   }
+const app = express();
+const port = 8080;
 
-// }
+app.use(express.urlencoded({ extended: true }));
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html; charset=utf-8' });
-
-  // if (req.url === '/result') res.end('<p>a</p>');
-  if (req.url === '/hello') res.end(`<p>こんにちわ</p>`);
-  if (req.url === '/redirect') res.writeHead(302, { location: '/redirected' });
-  if (req.url === '/redirected') res.end('<p>リダイレクトされました</p>');
-  if (req.url === '/') {
-    res.write(
-      `<a href="/result?param1=パラメータ1&param2=パラメータ2">Get Method Link</a>`
-    );
-    res.end(`
-      <form action="/result" method="POST">
-        <input type="text" name="title">
-        <input type="submit">
-      </form>
-    `);
-  } else {
-    if (req.method === 'GET') {
-      const queryString= req.url?.split("?")[1]
-      const params = new URLSearchParams(queryString);
-      // res.end(`${params}`)
-      console.log(params.get('param1'));
-    }
-    res.end(req.url);
-  }
+app.get('/', (req, res) => {
+  res.send(`
+    <a href="/result?param1=1&param2=2">Get Method Link</a>
+    <form action="/result" method="POST">
+      <p>title1: <input type="text" name="title[]"></p>
+      <p>title2: <input type="text" name="title[]"></p>
+      <p>description: <input type="text" name="description"></p>
+      <input type="submit">
+    </form>
+  `);
 });
 
-server.listen(8080);
+app.get(
+  '/result',
+  (req: Request<{}, {}, {}, { [key in 'param1' | 'param2']: string }>, res) => {
+    const params = req.query;
+    console.log(params);
+    res.send(`
+      <p>${params.param1}</p>
+      <p>${params.param2}</p>
+      `);
+  }
+);
+
+app.post(
+  '/result',
+  (req: Request<{}, {}, { title: string[]; description: string }>, res) => {
+    const params = req.body;
+    console.log(params);
+    res.send(`
+      <p>title1: ${params.title[0]}</p>
+      <p>title2: ${params.title[1]}</p>
+      <p>description: ${params.description}</p>
+    `);
+  }
+);
+
+
+
+
+
+// 必ず最後に配置すること
+app.listen(port, () => {
+  console.log(`Server start: http://localhost:${port}`);
+});
